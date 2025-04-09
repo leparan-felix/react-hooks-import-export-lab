c380acf5c09e4d582ebad56b39e566771033b0b6
leparan <leparanfelix16@gmail.com>
2025-04-09 12:10:36 +0300

    changes

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,11 +1,12 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import Home from "../components/Home";
+import Home from "../components/Home"; // correct import path
 
-test("it is exported as a default export", () => {
+test("it renders without crashing", () => {
   try {
     expect(() => render(<Home />)).not.toThrow();
   } catch (e) {
     throw new Error("Make sure to export this component!");
   }
 });
+ 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-import React from "react";
-
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
+      <h1 style={{ color: "firebrick" }}>
+        {/* insert username and city here if needed */}
       </h1>
     </div>
   );
 }
+
+export default Home;
